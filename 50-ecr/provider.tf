265e2e19dcde612428845671a21dd7458e5ff03a
Rushhav <rushav182@gmail.com>
2026-04-17 16:57:20 +0530

    expense-infra

---
+++ 50-ecr/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.28.0"
+    }
+  }
+    backend "s3" {
+    bucket         = "82s-tf-remote-state-dev-144"
+    key            = "expense-dev-ecr"
+    region         = "us-east-1"
+    dynamodb_table = "82s-tf-remote-state-dev-144"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
